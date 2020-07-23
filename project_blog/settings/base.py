9663a7cabd6e38c9db502111edd4d207e1c1acff
Ben <base_kh@hotmail.com>
2020-07-23 18:09:54 +0300

    heroku

--- project_blog/settings/base.py
+++ project_blog/settings/base.py
@@ -97,4 +97,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+print('STATIC_ROOT:', STATIC_ROOT)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
