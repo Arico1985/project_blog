1e16eefde1c71e2ada587d3897e290b2d07e3658
Ben <base_kh@hotmail.com>
2020-07-23 18:24:11 +0300

    heroku

--- project_blog/settings/base.py
+++ project_blog/settings/base.py
@@ -82,7 +82,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 TIME_ZONE = 'Europe/London'
 USE_I18N = True
 USE_L10N = True
@@ -91,7 +90,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
--- project_blog/wsgi.py
+++ project_blog/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "project_blog.settings.development")
+                      "project_blog.settings.production")
 
 application = get_wsgi_application()
